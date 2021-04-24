f307a2fe78c69951ff0fe87b0814777281bb41bb
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-04-24 14:47:40 +0530

    selectedChords, A-OKE

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 import Header from "./src/components/header";
-import Select from "./src/components/chordSelect";
+import ChordsSelect from "./src/components/chordSelect";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Select />
+      <ChordsSelect />
     </>
   );
-
-  // return (
-  //   <View style={styles.container}>
-  //     <Text>Open up App.js to start working on your app!</Text>
-  //     <StatusBar style="auto" />
-  //   </View>
-  // );
 }
 
 const styles = StyleSheet.create({
--- src/components/chordSelect.js
+++ src/components/chordSelect.js
@@ -1,42 +1,80 @@
-import React, { useState } from "react";
-import { TouchableOpacity, Text, View } from "react-native";
+import React, { Component } from "react";
+import { TouchableOpacity, Text, View, Button } from "react-native";
 
-const Select = () => {
-  const { textStyling, touchableOpacityNormalStyling, viewStyling } = styles;
-  const chords = ["A", "Am", "C", "D", "Dm", "E", "Em", "G"];
-  const [selectedChords, setSelectedChords] = useState([]);
+class ChordsSelect extends Component {
+  constructor(props) {
+    super(props);
+    const chords = ["A", "Am", "C", "D", "Dm", "E", "Em", "G"];
+    let selectedChords = [];
 
-  return (
-    <View style={viewStyling}>
-      {chords.map((chord, index) => (
-        <TouchableOpacity
-          key={index}
-          style={touchableOpacityNormalStyling}
-          onPress={() => {
-            console.log("------------------------------------------------");
-            console.log(
-              selectedChords.find((findChord) => findChord === chord)
-            );
-            if (selectedChords.find((findChord) => findChord === chord)) {
-              console.log(chord, "FOUND");
-              const index = selectedChords.indexOf(chord);
-              if (index > -1) {
-                selectedChords.splice(index, 1);
-              }
-              console.log("ALL CHORDS", selectedChords);
-            } else {
-              console.log(chord, "NOT FOUND");
-              setSelectedChords([...selectedChords, chord]);
-              console.log("ALL CHORDS", selectedChords);
+    for (i = 0; i < chords.length; i++) {
+      selectedChords = [
+        ...selectedChords,
+        { chordText: chords[i], isSelected: false },
+      ];
+    }
+
+    this.state = {
+      selectedChords: selectedChords,
+    };
+  }
+
+  confirmSelectedChords = () => {
+    console.log(
+      "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    );
+    for (i = 0; i < this.state.selectedChords.length; i++) {
+      if (this.state.selectedChords[i].isSelected) {
+        console.log(this.state.selectedChords[i].chordText, "SELECTED");
+      }
+    }
+  };
+
+  render = () => {
+    const {
+      textStyling,
+      touchableOpacityNormalStyling,
+      touchableOpacitySelectedStyling,
+      viewStyling,
+    } = styles;
+    return (
+      <View style={viewStyling}>
+        {this.state.selectedChords.map((chordObj, index) => (
+          <TouchableOpacity
+            key={index}
+            style={
+              chordObj.isSelected
+                ? touchableOpacitySelectedStyling
+                : touchableOpacityNormalStyling
             }
-          }}
-        >
-          <Text style={textStyling}>{chord}</Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
-};
+            onPress={() => {
+              console.log("--------------------------------------------------");
+              console.log(chordObj, "SELECTED");
+              this.setState((prevState) => {
+                let newSelectedChordsState = [...prevState.selectedChords];
+                let chordIndex = this.state.selectedChords.findIndex(
+                  (findChord) => findChord === chordObj
+                );
+                console.log(chordIndex);
+                let newSelectedChord = {
+                  ...newSelectedChordsState[chordIndex],
+                };
+                console.log("BEFORE UPDATE", newSelectedChord);
+                newSelectedChord.isSelected = !newSelectedChord.isSelected;
+                newSelectedChordsState[chordIndex] = newSelectedChord;
+                console.log("AFTER UPDATE", newSelectedChord);
+                return { selectedChords: newSelectedChordsState };
+              });
+            }}
+          >
+            <Text style={textStyling}>{chordObj.chordText}</Text>
+          </TouchableOpacity>
+        ))}
+        <Button title="Press me!" onPress={this.confirmSelectedChords} />
+      </View>
+    );
+  };
+}
 
 const styles = {
   textStyling: {
@@ -45,11 +83,16 @@ const styles = {
   touchableOpacityNormalStyling: {
     padding: 10,
     margin: 10,
+    backgroundColor: "gainsboro",
+  },
+  touchableOpacitySelectedStyling: {
+    padding: 10,
+    margin: 10,
     backgroundColor: "grey",
   },
   viewStyling: {
-    flex: 1,
-    flexDirection: "row",
+    // flex: 1,
+    // flexDirection: "row",
     backgroundColor: "gainsboro",
     height: "auto",
     alignItems: "center",
@@ -57,4 +100,4 @@ const styles = {
   },
 };
 
-export default Select;
+export default ChordsSelect;
