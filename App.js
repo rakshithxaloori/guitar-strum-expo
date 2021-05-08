155229a2e705e939c57f4391c3de271e38dd7dbf
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-05-08 16:24:50 +0530

    render countBar

--- App.js
+++ App.js
@@ -4,14 +4,12 @@ import { createStackNavigator } from "react-navigation-stack";
 import FirstScreenScreen from "./src/components/firstScreen";
 import SecondScreenScreen from "./src/components/secondScreen";
 import PlayScreen from "./src/components/play";
-import Test from "./src/components/test";
 
 const navigator = createStackNavigator(
   {
     Chords: FirstScreenScreen,
     StrummingPattern: SecondScreenScreen,
     Play: PlayScreen,
-    Test: Test,
   },
   {
     initialRouteName: "Chords",
