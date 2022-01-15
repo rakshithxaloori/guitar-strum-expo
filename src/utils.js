775dd527425c508455b242fa60eb931f5db3ead3
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-01-15 13:34:46 +0530

    upgrade expo, add bannerId env

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,6 @@
 // export const bannerId = "ca-app-pub-3940256099942544/6300978111"; // Test ID, Replace with your-admob-unit-id
 
 // Prod
-export const bannerId = "ca-app-pub-8538556251087145/7102435793";
+// export const bannerId = "ca-app-pub-8538556251087145/7102435793";
+
+export const bannerId = process.env.BANNER_ID;
