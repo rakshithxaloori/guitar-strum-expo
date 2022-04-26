4006b7edd3c2c04e8d66d536e0710cfdcd692ee7
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-04-26 09:43:24 +0530

    bash script

---
+++ prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Checkout to new branch
+git checkout -b production
+
+# Eject
+expo eject
+
+# Copy manifest files
+rsync -a manifest/manifest/android/ android/app/src/
+
+# Build the profile - KEY STEP (envs)
+eas build --profile production --platform android
+
+# Commit locally
+git add -A
+git commit -m "production"
+
+# Checkout to main
+git checkout main
+
+# Delete production branch
+git branch -D production
+
+# Delete android and ios directories
+rm -rf android
+rm -rf ios
